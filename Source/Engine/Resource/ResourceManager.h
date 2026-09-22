2d7359ef8500ad694c4200d8646b33a021ed1d05
tiresomenist <tiresomenist@gmail.com>
2026-09-22 20:35:22 +0900

    [feat] 비동기 로딩창/프리로딩 구현 완료
    이제 프리로드 될때까지 로딩창에서 기다림

--- Source/Engine/Resource/ResourceManager.h
+++ Source/Engine/Resource/ResourceManager.h
@@ -23,8 +23,26 @@
 #include "Core/Util/Objimporter.h"
 #include "Engine/Object/ObjectFactory.h"
 #include <wrl/client.h>
+#include <filesystem>
+#include <cstddef>
+#include <functional>
 
 class FTextureResource;
+
+// 프리로드 진행 상황과 최종 결과를 전달하며 엔진 리소스 포인터는 보관하지 않는다.
+struct FStaticMeshPreloadResult
+{
+	std::size_t TotalCount = 0;
+	std::size_t LoadedCount = 0;
+	std::size_t SkippedCount = 0;
+	std::size_t FailedCount = 0;
+	std::size_t SearchErrorCount = 0;
+
+	// 검색이 끝나야 TotalCount를 기준으로 진행률을 계산할 수 있다.
+	bool bSearchComplete = false;
+	bool bCancelled = false;
+	std::filesystem::path CurrentFile;
+};
 
 class GResourceManager
 {
@@ -42,16 +60,18 @@ public:
 	FFontAtlas* GetDefaultFont() { return DefaultFont.GetSRV() ? &DefaultFont : nullptr; }
 	FTextureResource* GetOrLoadTexture(const FString& FilePath);
 	UStaticMesh* GetOrLoadStaticMesh(const FName& MeshKey);
+	
+	// 일반 조회도 같은 키 해석과 로딩 경로를 사용한다.
 	UStaticMesh* GetStaticMesh(const FName& Key)
 	{
-		if (UStaticMesh** Found = StaticMeshCache.Find(Key))
-		{
-			return *Found;
-		}
 		return GetOrLoadStaticMesh(Key);
 	}
-	UStaticMesh* GetStaticMesh(const FString& FilePath) { return GetStaticMesh(FName(FilePath)); }
 
+	// 문자열 요청을 기존 FName 조회 경로로 전달한다.
+	UStaticMesh* GetStaticMesh(const FString& FilePath)
+	{
+		return GetStaticMesh(FName(FilePath));
+	}
 	void RegisterShader(const FName& Name,const WCHAR* FilePath,const char* VSEntry,
 		const char* PSEntry,const TArray<D3D11_INPUT_ELEMENT_DESC>& Layout);
 	void RegisterBlendState(const FName& Name,const D3D11_BLEND_DESC& Desc);
@@ -72,7 +92,11 @@ public:
 	void RegisterDepthStencilState(const FName& Name, const D3D11_DEPTH_STENCIL_DESC& Desc);
 	ID3D11DepthStencilState* GetDepthStencilState(const FName& Name) const;
 
-
+	// 메인 스레드에서 바이너리 캐시만 미리 로딩하고 진행 상황과 취소 요청을 처리한다.
+	FStaticMeshPreloadResult PreloadCachedStaticMeshes(
+		const std::filesystem::path& Root,
+		const std::function<void(const FStaticMeshPreloadResult&)>& OnProgress = {},
+		const std::function<bool()>& ShouldCancel = {});
 private:
 	GResourceManager() = default;
 	~GResourceManager() = default;
@@ -82,6 +106,8 @@ private:
 	void RegisterDefaultRasterizerStates();
 	void RegisterDefaultBlendStates();
 	void RegisterDefaultDepthStencilStates();
+	// 정규화한 절대 OBJ 경로를 기준으로 CPU 데이터를 GPU 메시로 만들고 등록한다.
+	UStaticMesh* BuildAndCacheStaticMesh(const std::filesystem::path& ObjPath, FStaticMeshData& MeshData);
 	Microsoft::WRL::ComPtr<ID3D11Buffer> TextureMaterialConstantBuffer;
 	Microsoft::WRL::ComPtr<ID3D11PixelShader> WireframePixelShader;
 	TMap<FName, Microsoft::WRL::ComPtr<ID3D11RasterizerState>> RasterizerStateCache;
@@ -93,7 +119,10 @@ private:
 
 	TMap<FName, FMeshResource*> PrimitiveCache;
 	TMap<FString, FTextureResource*> TextureCache;
+	// 정규화한 절대 OBJ 경로로 완성된 메시를 조회한다.
 	TMap<FName, UStaticMesh*> StaticMeshCache;
+	// 성공한 요청의 원래 키를 절대 경로 키에 연결하여 반복적인 파일 검사를 피한다.
+	TMap<FName, FName> StaticMeshAliases;
 	FFontAtlas DefaultFont;
 };
 
